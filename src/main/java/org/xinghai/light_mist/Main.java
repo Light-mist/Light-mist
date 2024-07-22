a6f06ecfa11d8a9b7ab83eea8c7a4681a024d1e0
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-07-22 13:20:58 +0800

    修改 执行逻辑。

--- src/main/java/org/xinghai/light_mist/Main.java
+++ src/main/java/org/xinghai/light_mist/Main.java
@@ -1,42 +1,40 @@
-package org.xinghai.light_mist;
-
-import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONArray;
-import com.alibaba.fastjson2.JSONObject;
-
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Map;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
-import org.xinghai.light_mist.ReadFile;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
 public class Main {
     public static void main(String[] args) {
-        String jsonValue;
-        jsonValue = ReadFile.get_file("config/data.json");
-        if (ReadFile.code == 1){
-            System.out.println("出现错误！来自文件读取，错误内容："+jsonValue);
-        }
-
-        List<JSONObject> jsonArray = JSON.parseArray(jsonValue, JSONObject.class);
+        String file = "config/data.json";
         List<Integer> names = new ArrayList<>();
-        try {
-            jsonArray = mapper.readValue(jsonValue, List.class);
-            for (Map<String, Object> obj : jsonArray) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file)))
+        {
+            String jsonString;
+            StringBuilder jsonContent = new StringBuilder();
+            // 逐行读取文件内容
+            while ((jsonString = reader.readLine()) != null) {
+                jsonContent.append(jsonString);
+            }
+            JSONArray jsonArray = JSON.parseArray(jsonContent.toString());
+
+            for (int i = 0; i < jsonArray.size(); i++) {
+                JSONObject obj = jsonArray.getJSONObject(i);
                 System.out.println("启动: " + obj.getString("name"));
                 String runvalue = obj.getString("command");
                 String dirPath = obj.getString("dir");
                 File rundir = new File(dirPath);
-                names.add(Integer.valueOf(obj.getString("name")));
+                names.add(obj.getIntValue("name"));
                 Process process = Runtime.getRuntime().exec(runvalue, null, rundir);
             }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        System.out.println("已成功执行所有指令，列表为：" + names);
+        System.out.println("已成功执行所有指令，列表为："+names);
     }
 }
