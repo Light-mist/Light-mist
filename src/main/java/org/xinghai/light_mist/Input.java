6e85f9e4ce7ce305ff2793de59a0f3737db2e92b
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-07-23 12:27:48 +0800

    修复 读取不完整。

---
+++ src/main/java/org/xinghai/light_mist/Input.java
@@ -0,0 +1,10 @@
+package org.xinghai.light_mist;
+
+import java.util.Scanner;
+
+public class Input {
+    public static String input(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
