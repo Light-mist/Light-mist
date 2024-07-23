e5bb7a516a79d12ddd38e85b382c815628663e86
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-07-24 00:05:12 +0800

    新增 命令执行。

--- src/main/java/org/xinghai/light_mist/Main.java
+++ src/main/java/org/xinghai/light_mist/Main.java
@@ -1,18 +1,19 @@
 package org.xinghai.light_mist;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import org.xinghai.light_mist.RunCommand;
-import org.xinghai.light_mist.Input;
-import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONArray;
-import com.alibaba.fastjson2.JSONObject;
+import java.util.List;
 
 public class Main {
+    static JSONArray jsonArray = null;
     public static void main(String[] args) {
         String file = "config/light_mist.json";
         List<String> names = new ArrayList<>();
@@ -24,7 +25,7 @@ public class Main {
             while ((jsonString = reader.readLine()) != null) {
                 jsonContent.append(jsonString);
             }
-            JSONArray jsonArray = JSON.parseArray(jsonContent.toString());
+            jsonArray = JSON.parseArray(jsonContent.toString());
 
             for (int i = 0; i < jsonArray.size(); i++) {
                 JSONObject obj = jsonArray.getJSONObject(i);
@@ -38,13 +39,36 @@ public class Main {
                     System.out.println("抱歉！暂不支持此系统！");
                 }
             }
-            String get_input = Input.input();
-            if(get_input=="exit"){
-                return;
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
         System.out.println("已成功执行所有指令，列表为："+names);
+        String get_input = Input.input();
+        if (get_input.equals("exit")) {
+            System.err.println("正在退出中...");
+        } else if (get_input.startsWith("run")) {
+            String[] parts = get_input.split(" ");
+            if (parts.length >= 3) {
+                try {
+                    String dirPath = parts[1];
+                    String command = parts[2];
+                    File rundir = new File(dirPath);
+                    command = command+" > log.txt";
+                    Process run_return = RunCommand.run(rundir, command);
+                    try {
+                        int exitVal = run_return.waitFor();
+                        System.out.println(exitVal == 0 ? "执行成功！" : "执行失败");
+                        String s = Files.readString(Paths.get(rundir+"log.txt"));
+                        System.out.println(s);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            } else {
+                System.err.println("错误：指令错误!");
+            }
+        }
     }
 }
